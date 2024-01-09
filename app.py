73067eae59e69755e43d16074f879eb129af7499
Sebastian Vargr <sebbdk@users.noreply.github.com>
2024-01-09 10:28:53 +0100

    simplify

--- app.py
+++ app.py
@@ -5,26 +5,19 @@ import numpy as np
 #training_data, validation_data, test_data = mnist_loader.load_data_wrapper('./mnist/data/mnist.pkl.gz')
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper_pickled()
 
+print(training_data[0])
+
 def npRelu(x):
     return np.where(x > 0, x, 0)
 
 def reluPrime(x):
     return np.where(x >= 0, 1, 0.01)
 
-def ReLU(x):
-    return x * (x > 0)
 
-def dReLU(x):
-    return 0.01 * (x > 0)
+#dl.save_mnist()
+#net = network.Network([784, 32, 10], acti=npRelu, actiPrime=reluPrime, showProgress=True)
+net = network.Network([784, 32, 10], acti=npRelu, actiPrime=reluPrime)
+#net = network.Network([784, 30, 10])
 
-def run():
-    #dl.save_mnist()
-    #net = network.Network([784, 32, 10], acti=npRelu, actiPrime=reluPrime, showProgress=True)
-    #net = network.Network([784, 32,16, 10], acti=npRelu, actiPrime=reluPrime)
-    net = network.Network([784, 30, 10])
-
-    net.SGD(training_data, 10, 10, 3, test_data=test_data, debug=True)
-    #net.SGD(training_data, 500, 10, 0.01, test_data=test_data)
-
-
-run()
+#net.SGD(training_data, 30, 10, 3, test_data=test_data, debug=True)
+net.SGD(training_data, 500, 10, 0.01, test_data=test_data, debug=True)
