b7d718f011c41433a27ced8e2404ba66686e5447
Sebastian Vargr <870110+sebbdk@users.noreply.github.com>
2024-01-09 13:40:53 +0100

    initial code pytorch

--- pytorch/sebastian/__init__.py
+++ pytorch/sebastian/__init__.py
@@ -4,3 +4,17 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda
 import matplotlib.pyplot as plt
+
+training_data = datasets.MNIST(
+    root='data',
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.MNIST(
+    root='data',
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
