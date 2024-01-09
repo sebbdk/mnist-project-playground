a69ae9201b3ee8f59c8dbe7ba39e73ad62969cba
Sebastian Vargr <870110+sebbdk@users.noreply.github.com>
2024-01-09 11:28:33 +0100

    removed numba

--- network.py
+++ network.py
@@ -12,18 +12,15 @@ and omits many desirable features.
 #### Libraries
 # Standard library
 import random
-from numba import jit
 
 # Third-party libraries
 import numpy as np
 #### Miscellaneous functions
 
-@jit(nopython=True)
 def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
 
-@jit(nopython=True)
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
